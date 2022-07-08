4f0ee669db7fe5dcd1c164c8cba474e3026870da
Priyosaha <107949592+Priyosaha@users.noreply.github.com>
2022-07-08 22:12:51 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,20 @@
 // complete the given function
 
 function palindrome(str){
+  
+  
+str=str.toLowerCase();
+let i=0;
+let j=str.length-1;
+while(i<j){
+    if(str.charAt(i)==str.charAt(j)){
+        i++;
+        j--;
+    }else{
+       return false;
+    }
+}
+return true;
 
 }
 module.exports = palindrome
